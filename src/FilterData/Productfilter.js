99dce3a0ed93edc259c81081fac2e749711b2f1d
vivignutz <vgnutzmann@gmail.com>
2023-12-06 08:47:43 +0100

    Changed class to className at Productfilter.js

--- src/FilterData/Productfilter.js
+++ src/FilterData/Productfilter.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import Product from '../Product.json'
 
 
+// Principal component to filter and show products
 export default function Productfilter() {
     const [items, setItems] = useState(Product);
     console.log(items)
 
+    // Filter by category
     const filterItems = (catItem) => {
         const updateItems = Product.filter((curItem) => {
             return curItem.category === catItem
@@ -13,23 +15,27 @@ export default function Productfilter() {
         setItems(updateItems);
     }
 
+    // Buttons as a filter
     return (
         <div className='container mt-5'>
             <h1>All products</h1>
-            <button type="button" class="btn btn-success me-3" onClick={() => setItems(Product)}>All Products</button>
-            <button type="button" class="btn btn-success me-3" onClick={() => filterItems("women's clothing")}>Women's Clothing</button>
-            <button type="button" class="btn btn-success me-3" onClick={() => filterItems("men's clothing")}>Men's Clothing</button>
-            <button type="button" class="btn btn-success me-3" onClick={() => filterItems("jewelery")}>Jewelery</button>
-            <button type="button" class="btn btn-success me-3" onClick={() => filterItems("electronics")}>Electronics</button>
+            <button type="button" className="btn btn-success me-3" onClick={() => setItems(Product)}>All Products</button>
+            <button type="button" className="btn btn-success me-3" onClick={() => filterItems("women's clothing")}>Women's Clothing</button>
+            <button type="button" className="btn btn-success me-3" onClick={() => filterItems("men's clothing")}>Men's Clothing</button>
+            <button type="button" className="btn btn-success me-3" onClick={() => filterItems("jewelery")}>Jewelery</button>
+            <button type="button" className="btn btn-success me-3" onClick={() => filterItems("electronics")}>Electronics</button>
 
             <hr />
+            {/* Show filtered items */}
             <div className='row justify-content-center'>
-                {items.map((val)=>(
-                    <div className = 'col-md-3'>
-                        <div class="card">
-                            <img src={val.image} class="card-img-top img-fluid" alt="..." />
-                                <div class="card-body">
-                                <h5>{val.title.substring(0,18)} <span class="badge bg-primary">{val.category}</span></h5>
+                {items.map((val, index)=>(
+                    <div key={index} className='col-md-3'>
+                        <div className="card">
+                            <img src={val.image} className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+
+                                {/* Product title has max. 18 characters */}
+                                <h5>{val.title.substring(0,18)} <span className="badge bg-primary">{val.category}</span></h5>
                                 
                                 </div>
                         </div>                                            
